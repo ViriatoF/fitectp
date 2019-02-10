30b3a375ca52ff63a757ffed92e7464f52cc2c3d
ViriatoF <https://github.com/ViriatoF>
2019-02-10 14:29:00 +0100

    modif Api, routes and layout + add 2 viewModels (EnrollmentVM, StudentsApi)

--- src/ContosoUniversity/App_Start/RouteConfig.cs
+++ src/ContosoUniversity/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace ContosoUniversity
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "NewEnrollment",
+                url:"Student/NewEnrollment/{id}",
+                defaults: new { controller = "Enrollments", action = "Create", id = UrlParameter.Optional});
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ src/ContosoUniversity/ViewModels/EnrollmentVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class EnrollmentVM
+    {
+        public int courseId { get; set; }
+    }
+}
